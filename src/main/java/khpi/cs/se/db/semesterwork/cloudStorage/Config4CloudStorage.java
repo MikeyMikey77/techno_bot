8c9daab3ba4c2332b8d3b6dce3bf2fe07c1738b6
MikeyMikey77 <57002133+MikeyMikey77@users.noreply.github.com>
2020-12-06 22:07:59 +0200

    Update Config4CloudStorage.java

--- src/main/java/khpi/cs/se/db/semesterwork/cloudStorage/Config4CloudStorage.java
+++ src/main/java/khpi/cs/se/db/semesterwork/cloudStorage/Config4CloudStorage.java
@@ -2,9 +2,9 @@ package khpi.cs.se.db.semesterwork.cloudStorage;
 
 public class Config4CloudStorage {
 
-    public static final String CLOUD_NAME = "student-khpi";
+    public static final String CLOUD_NAME = "dtritus";
 
-    public static final String API_KEY = "629121664633488";
+    public static final String API_KEY = "798327362768793";
 
-    public static final String API_SECRET = "2AxgQ-hRcHgeN2VlOJMdSIk4XrE";
+    public static final String API_SECRET = "wAwfpjMPdmQb5s6LHcSOatUvGX0";
 }
