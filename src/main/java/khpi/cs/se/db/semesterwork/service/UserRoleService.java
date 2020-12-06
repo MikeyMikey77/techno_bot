86cd2d2ca1ef4057c4f05389bec059b09393973c
MikeyMikey77 <57002133+MikeyMikey77@users.noreply.github.com>
2020-12-06 23:23:45 +0200

    Update UserRoleService.java

--- src/main/java/khpi/cs/se/db/semesterwork/service/UserRoleService.java
+++ src/main/java/khpi/cs/se/db/semesterwork/service/UserRoleService.java
@@ -21,35 +21,35 @@ public class UserRoleService {
         this.userRepository = userRepository;
     }
 
-//    @PostConstruct
-//    public void init(){
-//        if(repository.count() == 0) {
-//            UserRole role1 = new UserRole();
-//            role1.setTitle("ROLE_ADMIN");
-//            role1.setDiscount(30);
-//            repository.save(role1);
-//            UserRole role2 = new UserRole();
-//            role2.setTitle("ROLE_CUSTOMER10");
-//            role2.setDiscount(5);
-//            repository.save(role2);
-//            UserRole role3 = new UserRole();
-//            role3.setTitle("ROLE_CUSTOMER50");
-//            role3.setDiscount(10);
-//            repository.save(role3);
-//            UserRole role4 = new UserRole();
-//            role4.setTitle("ROLE_CUSTOMER100");
-//            role4.setDiscount(15);
-//            repository.save(role4);
-//            UserRole role5 = new UserRole();
-//            role5.setTitle("ROLE_CUSTOMER500");
-//            role5.setDiscount(20);
-//            repository.save(role5);
-//            UserRole role6 = new UserRole();
-//            role6.setTitle("ROLE_CUSTOMER-VIP");
-//            role6.setDiscount(25);
-//            repository.save(role6);
-//        }
-//    }
+   @PostConstruct
+   public void init(){
+       if(repository.count() == 0) {
+           UserRole role1 = new UserRole();
+           role1.setTitle("ROLE_ADMIN");
+           role1.setDiscount(30);
+           repository.save(role1);
+           UserRole role2 = new UserRole();
+           role2.setTitle("ROLE_CUSTOMER10");
+           role2.setDiscount(5);
+           repository.save(role2);
+           UserRole role3 = new UserRole();
+           role3.setTitle("ROLE_CUSTOMER50");
+           role3.setDiscount(10);
+           repository.save(role3);
+           UserRole role4 = new UserRole();
+           role4.setTitle("ROLE_CUSTOMER100");
+           role4.setDiscount(15);
+           repository.save(role4);
+           UserRole role5 = new UserRole();
+           role5.setTitle("ROLE_CUSTOMER500");
+           role5.setDiscount(20);
+           repository.save(role5);
+           UserRole role6 = new UserRole();
+           role6.setTitle("ROLE_CUSTOMER-VIP");
+           role6.setDiscount(25);
+           repository.save(role6);
+       }
+   }
 
     public boolean isEmpty(){
         if(repository.count() == 0){
